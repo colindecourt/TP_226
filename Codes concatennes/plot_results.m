ee2aa180f9563e13869de8c1b78d96c10912c8fd
DecourtC <decourtcolin@gmail.com>
2018-12-03 17:48:39 +0100

    Fin code concaténés
    Manque 2 mesures

--- Codes concatennes/plot_results.m
+++ Codes concatennes/plot_results.m
@@ -1,8 +1,7 @@
 close all
 figure(1)
-NC1= load('NC1_fermé.mat');
-NC2= load('NC2_fermé.mat');
-NC3= load('NC3_fermé.mat');
+NC1= load('NC_intro.mat');
+NC2= load('NC_sansentrelaceur.mat');
 NC4= load('NC4_fermé.mat');
 NC = load('NC.mat');
 
@@ -11,7 +10,6 @@ semilogy(NC.EbN0dB,NC.Pb);
 hold on
 semilogy(NC1.EbN0dB,NC1.ber);
 semilogy(NC2.EbN0dB,NC2.ber);
-semilogy(NC3.EbN0dB,NC3.ber);
 semilogy(NC4.EbN0dB,NC4.ber);
 
 
@@ -21,5 +19,5 @@ ylim([1e-6 1])
 grid on
 xlabel('$\frac{E_b}{N_0}$ en dB','Interpreter', 'latex', 'FontSize',14)
 ylabel('TEB','Interpreter', 'latex', 'FontSize',14)
-legend({'Peb non codee','Code (2,3)','Code (7,5)','Code (13,15)','Code (133,171)'}, 'Interpreter', 'latex', 'FontSize',11);
+legend({'Peb non codee','Code concatene avec entrelaceur et codeur RS','Code concatene sans entrelaceur et codeur RS','Code (133,171)'}, 'Interpreter', 'latex', 'FontSize',11);
 title('TEB en fonction de Eb/N0 pour chacun des codes');
